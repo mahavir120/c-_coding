95fa7b4e9862ba947a4d5b52573a2e2b55b72cae
mahavir <mahaviroraonjsr@gmail.com>
2022-10-18 23:22:42 +0530

    var1 is added in the github

---
+++ var1.cpp
@@ -0,0 +1,17 @@
+// All type of variables are declared
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int data;
+    float price;
+    string name;
+    char ch;
+    double rate;
+    bool status;
+
+    return 0;
+    
+
+}
