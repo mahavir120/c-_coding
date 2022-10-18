1e892dd1f2476441dd4342b5f55d4608941da022
mahavir <mahaviroraonjsr@gmail.com>
2022-10-18 23:52:55 +0530

    looping in c++

---
+++ looping.cpp
@@ -0,0 +1,13 @@
+// loom demo program
+#include<iostream>
+using namespace  std;
+int main(){
+    int data;
+    // working with while loop
+    cin>>data;
+    while(data>0){
+        cout<<data<<" ";
+        data--;
+
+    }
+}
