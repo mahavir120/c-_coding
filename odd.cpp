6d4882c5d1b999d8a17b9f72f2c286859a8c02a5
mahavir <mahaviroraonjsr@gmail.com>
2022-10-18 23:40:47 +0530

    odd value testing

---
+++ odd.cpp
@@ -0,0 +1,13 @@
+// checking odd number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int data;
+    cin>>data;
+    if(data%2!=0){
+        cout<<"odd"<<endl;
+    }
+    return 0;
+}
