da83b12289c916c4c5c54dd543198bf3f392603c
mahavir <mahaviroraonjsr@gmail.com>
2022-10-18 23:48:44 +0530

    prime number

---
+++ prime.cpp
@@ -0,0 +1,20 @@
+// testing number is prime or not
+#include<iostream>
+using namespace std;
+
+int main(){
+    int data;
+    cin>>data;
+    bool flag=false;
+    for(int i=2;i<data;i++){
+        if(data%i==0){
+            cout<<"not a prime number"<<endl;
+            flag-true;
+
+            break;
+        }
+    }
+    if(flag==false){
+        cout<<"prime number"<<endl;
+    }
+}
