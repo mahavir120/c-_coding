dc46a0fcd9aa4f7cc771827b64d749bc1c7a13b3
mahavir <mahaviroraonjsr@gmail.com>
2022-10-18 23:43:01 +0530

    even number testing

---
+++ even.cpp
@@ -0,0 +1,15 @@
+// testing number is even
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int data;
+    cin>>data;
+    if(data%2==0){
+        cout<<"even"<<endl;
+    }
+
+    return 0;
+    
+}
